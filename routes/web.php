54f04125dc81c17b789df4c8f69216e434024abd
arunakumari.gadi <arunakumari.gadi@karkinos.in>
2024-03-15 23:30:03 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\ApplicationController;
+Route::get('application','App\Http\Controllers\ApplicationController@index')->name('index');
+Route::post('save','App\Http\Controllers\ApplicationController@store')->name('datastore');
+Route::get('tests/{id}', 'App\Http\Controllers\ApplicationController@show')->name('applicants.show');
+Route::post('submit-answers', 'App\Http\Controllers\ApplicationController@submitAnswers');
+
+Route::get('/', function () {
+    return view('welcome');
+});
